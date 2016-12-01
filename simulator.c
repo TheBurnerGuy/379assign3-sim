626d3c2ded30016e75b8bae6824830105b710c15
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-12-01 16:26:15 -0700

    reverted changes

--- simulator.c
+++ simulator.c
@@ -1,13 +1,7 @@
-typedef struct node_{
-	int num;
-	struct node_* next;
-	struct node_* prev;
-}node;
-
 void init(int psize, int winsize){
 	int* mem = (int*)malloc((2^25)/sizeof(int));
-	int wsModelsHead; //need to implement 2 way linked list
-	int wsModelsTail;
+	int* set = (int*)malloc(winsize); //Note: Should make this a hash table
+	int* workingsetmodels; //2d array to keep track of all working sets so far?	
 	int counter = 0;
 	int pagesize = psize;
 	int windowsize = winsize;
@@ -16,11 +10,55 @@ void init(int psize, int winsize){
 void put(unsigned int address, int value){
 	mem[address] = value;
 	int pageNum = (i*32)/psize;
-	
+	//check if pageNum is in current working set
+	isInSet = 0;
+	for(int i = 0; i < winsize; ++i){ //change winsize later to actual size of set
+		if(set[i]==pageNum){
+			//Base case: pageNum is in the set
+			isInSet = 1;
+			break;
+		}
+	}
+	if(!isInSet){
+		//put it in the set - nothing here yet!
+	}
+	//check if counter has reached windowsize
+	counter+=1;
+	if (counter==winsize){
+		//copy & store working set into some stash somewhere
+		for(i = 0; i < len(set[i]); ++i){
+			workingsetmodels[amountofws][i] = set[i];
+			//also clear the set
+			set[i] = 0;
+		}		
+	}
 }
 
-int get(unsigned int address){
+void get(unsigned int address){
 	int pageNum = (i*32)/psize;
+	//check if pageNum is in current working set
+	isInSet = 0;
+	int i;
+	for(i = 0; i < winsize; ++i){ //change winsize later to actual size of set
+		if(set[i]==pageNum){
+			//Base case: pageNum is in the set
+			isInSet = 1;
+			break;
+		}
+	}
+	if(!isInSet){
+		//put it in the set - nothing here yet!
+	}
+	//check if counter has reached windowsize
+	counter+=1;
+	if (counter==winsize){
+		//copy & store working set into some stash somewhere
+		for(i = 0; i < len(set[i]); ++i){
+			workingsetmodels[amountofws][i] = set[i];
+			//also clear the set
+			set[i] = 0;
+		}		
+	}
 	return mem[address];
 }
 
@@ -28,6 +66,6 @@ void done(){
 	//print all working sets - unfinished
 	int i;
 	for(i = 0; i<len(workingsetmodels); ++i){
-		print(workingsetmodels[i]);//should print linked lists so change this later
+		print(workingsetmodels[i]);
 	}
 }
