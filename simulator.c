24bf59249cdc308f1d0f067b83c7935fec140546
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-12-01 16:04:03 -0700

    changes in design of code
    yarr

--- simulator.c
+++ simulator.c
@@ -1,7 +1,13 @@
+typedef struct node_{
+	int num;
+	struct node_* next;
+	struct node_* prev;
+}node;
+
 void init(int psize, int winsize){
 	int* mem = (int*)malloc((2^25)/sizeof(int));
-	int* set = (int*)malloc(winsize); //Note: Should make this a hash table with # of buckets = winsize/4. Makes it easy to find if number is in hash table 
-	int* workingsetmodels; //need to implement 2 way linked list	
+	int wsModelsHead; //need to implement 2 way linked list
+	int wsModelsTail;
 	int counter = 0;
 	int pagesize = psize;
 	int windowsize = winsize;
@@ -10,55 +16,11 @@ void init(int psize, int winsize){
 void put(unsigned int address, int value){
 	mem[address] = value;
 	int pageNum = (i*32)/psize;
-	//check if pageNum is in current working set
-	isInSet = 0;
-	for(int i = 0; i < winsize; ++i){ //change winsize later to actual size of set
-		if(set[i]==pageNum){
-			//Base case: pageNum is in the set
-			isInSet = 1;
-			break;
-		}
-	}
-	if(!isInSet){
-		//put it in the set - nothing here yet!
-	}
-	//check if counter has reached windowsize
-	counter+=1;
-	if (counter==winsize){
-		//copy & store working set into some stash somewhere
-		for(i = 0; i < len(set[i]); ++i){
-			workingsetmodels[amountofws][i] = set[i];
-			//also clear the set
-			set[i] = 0;
-		}		
-	}
+	
 }
 
 int get(unsigned int address){
 	int pageNum = (i*32)/psize;
-	//check if pageNum is in current working set
-	isInSet = 0;
-	int i;
-	for(i = 0; i < winsize; ++i){ //change winsize later to actual size of set
-		if(set[i]==pageNum){
-			//Base case: pageNum is in the set
-			isInSet = 1;
-			break;
-		}
-	}
-	if(!isInSet){
-		//put it in the set - nothing here yet!
-	}
-	//check if counter has reached windowsize
-	counter+=1;
-	if (counter==winsize){
-		//copy & store working set into some stash somewhere
-		for(i = 0; i < len(set[i]); ++i){
-			workingsetmodels[amountofws][i] = set[i];
-			//also clear the set
-			set[i] = 0;
-		}		
-	}
 	return mem[address];
 }
 
