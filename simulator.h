b00367e1af33c3752759d8be312491b2e9e099ac
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-12-02 00:42:50 -0700

    Create simulator.h
    added comments and yadda

---
+++ simulator.h
@@ -0,0 +1,22 @@
+//init() initializes variables in the simulator
+//Variables include the memory for all the pages, the working set, linked list for all recorded working sets, etc.
+void init(int psize, int winsize);
+
+//setCheck() is a helper function for put() and get()
+//Given a page number, checks if the page number is already in the current working set
+//If this memory reference is equal to the window size, send current working set to the linked list of all recorded working sets
+void setCheck(int pageNum);
+
+//put()
+//Given an address to an integer that is between 0 and 2^25-1, places the given value in that address
+void put(unsigned int address, int value);
+
+//get()
+//Given an address to an integer that is already stored in the simulator by put(), return that integer.
+int get(unsigned int address);
+
+//done()
+//called by process(), parameter is head node of the linked list of ws sizes
+//print to stdout: list of working set sizes quantized to interval
+//				   average of working set size
+void done(struct node* workingsetmodels);
