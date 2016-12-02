e4d173d3c83b844bf4996069ee27fda2d208fd28
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-12-02 00:43:35 -0700

    included code from done, also bug fixes - untested
    main priority is to test using a sort?

--- simulator.c
+++ simulator.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 //Linked list implementation
 typedef struct node_{
 	int num;
@@ -12,7 +15,7 @@ node* addNode(node* tailNode, int number){
 		exit(1);
 	}
 	tailNode->next->num = number;
-	return newNode;
+	return tailNode->next;
 }
 
 //Deletes all nodes
@@ -27,58 +30,79 @@ void deleteNodes(node* nodeHead){
 }
 
 //Simulator implementation
+
+//Global variables
+int* mem;
+int* set;
+node* ws_head; //Head node of Linked list for all working sets contained so far
+node* ws_tail;
+int counter;
+int pagesize;
+int windowsize;
+
 void init(int psize, int winsize){
-	static int* mem = (int*)malloc((2^25)/sizeof(int));
-	static int* set = (int*)calloc(winsize,sizeof(int));
-	static node* ws_head = (node*)malloc(sizeof(node)); //Head node of Linked list for all working sets contained so far
-	static node* ws_tail = ws_Head;	
-	static int counter = 0;
-	static int pagesize = psize;
-	static int windowsize = winsize;
+	mem = (int*)malloc((2^25)/sizeof(int));
+	set = (int*)calloc(winsize,sizeof(int));
+	ws_head = (node*)malloc(sizeof(node)); //Head node of Linked list for all working sets contained so far
+	ws_tail = ws_head;	
+	counter = 0;
+	pagesize = psize;
+	windowsize = winsize;
 }
 
-//Checks if page is inside of set and also checks if counter has reached the window size interval
-//Function created for convenience instead of writing to both put and get
+//setCheck - fundamental piece of put() and get()
+//Checks if page is inside of set (current working set interval) and also checks if counter has reached the window size interval
+//If counter has reached ws interval, put current set into a linked list and clear the set for new interval
 void setCheck(int pageNum){
 	//check if pageNum is in current working set
-	isInSet = 0;
+	int temp = 0; //Right now, used to check if a pageNum is inside current working set
 	int i;
-	for(i = 0; i < winsize && set[i] != 0; ++i){ //change winsize later to actual size of set
+	for(i = 0; i < windowsize && set[i] != 0; ++i){
 		if(set[i]==pageNum){
 			//Base case: pageNum is in the set
-			isInSet = 1;
+			temp = 1;
 			break;
 		}
 	}
-	if(!isInSet) set[i] = pageNum;
+	if(!temp) set[i] = pageNum;
 	//check if counter has reached windowsize
 	counter+=1;
-	if (counter==winsize){
+	if (counter==windowsize){
 		//copy & store working set size
-		int length = 0;
-		for(i = 0; i < winsize && set[i] != 0; ++i){
-			++length;
+		int temp = 0; //Now temp is used to store the size of of set[i]
+		counter = 0;
+		for(i = 0; i < windowsize && set[i] != 0; ++i){
+			++temp;
 			//also clear the set
 			set[i] = 0;
 		}
-		ws_tail = addNode(ws_tail, length);	
+		ws_tail = addNode(ws_tail, temp);	
 	}
 }
 
 void put(unsigned int address, int value){
 	mem[address] = value;
-	setCheck(1+(address*32)/pagesize);
+	setCheck(1+((address*32)/pagesize));
 }
 
-void get(unsigned int address){
-	setCheck(1+(address*32)/pagesize);
+int get(unsigned int address){
+	setCheck(1+((address*32)/pagesize));
 	return mem[address];
 }
 
-void done(){
-	//print all working sets - unfinished
-	int i;
-	for(i = 0; i<len(workingsetmodels); ++i){
-		print(workingsetmodels[i]);
-	}
+//called by process(), parameter is head node of the linked list of ws sizes
+//print to stdout: list of working set sizes quantized to interval
+//				   average of working set size
+void done(struct node* workingsetmodels) {
+    struct node* current = workingsetmodels;
+    int i = 0;
+    int sum = 0;
+    while (current!= NULL) {
+    	i++;
+    	printf("T%d: %d\n", i, current->num);
+    	sum += current ->num;
+
+    	current = current-> next;
+    }
+    printf("Average Working Set Size: %d\n\n", sum/i);	
 }
