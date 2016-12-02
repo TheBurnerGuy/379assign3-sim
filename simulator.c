413e8925e586ebcaf9c71dc7d23cae7fd762a04c
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-12-01 17:27:28 -0700

    completed simulator.c - untested
    added new functions addNode, clearNodes, setCheck
    struct Node added

--- simulator.c
+++ simulator.c
@@ -1,64 +1,77 @@
+//Linked list implementation
+typedef struct node_{
+	int num;
+	struct node_* next;
+}node;
+
+//Adds a node to tail node, and returns new tail node
+node* addNode(node* tailNode, int number){
+	tailNode->next = (node*)malloc(sizeof(node));
+	if(tailNode->next == NULL){
+		perror("not enough memory to allocate more space for llist"); 
+		exit(1);
+	}
+	tailNode->next->num = number;
+	return newNode;
+}
+
+//Deletes all nodes
+//Postcondition: nodeHead points to an unknown address
+void deleteNodes(node* nodeHead){
+	node* currentNode = nodeHead;
+	while(nodeHead!=NULL){
+		currentNode = nodeHead;
+		nodeHead = nodeHead->next;
+		free(currentNode);
+	}
+}
+
+//Simulator implementation
 void init(int psize, int winsize){
-	int* mem = (int*)malloc((2^25)/sizeof(int));
-	int* set = (int*)malloc(winsize); //Note: Should make this a hash table
-	int* workingsetmodels; //2d array to keep track of all working sets so far?	
-	int counter = 0;
-	int pagesize = psize;
-	int windowsize = winsize;
+	static int* mem = (int*)malloc((2^25)/sizeof(int));
+	static int* set = (int*)calloc(winsize,sizeof(int));
+	static node* ws_head = (node*)malloc(sizeof(node)); //Head node of Linked list for all working sets contained so far
+	static node* ws_tail = ws_Head;	
+	static int counter = 0;
+	static int pagesize = psize;
+	static int windowsize = winsize;
+}
+
+//Checks if page is inside of set and also checks if counter has reached the window size interval
+//Function created for convenience instead of writing to both put and get
+void setCheck(int pageNum){
+	//check if pageNum is in current working set
+	isInSet = 0;
+	int i;
+	for(i = 0; i < winsize && set[i] != 0; ++i){ //change winsize later to actual size of set
+		if(set[i]==pageNum){
+			//Base case: pageNum is in the set
+			isInSet = 1;
+			break;
+		}
+	}
+	if(!isInSet) set[i] = pageNum;
+	//check if counter has reached windowsize
+	counter+=1;
+	if (counter==winsize){
+		//copy & store working set size
+		int length = 0;
+		for(i = 0; i < winsize && set[i] != 0; ++i){
+			++length;
+			//also clear the set
+			set[i] = 0;
+		}
+		ws_tail = addNode(ws_tail, length);	
+	}
 }
 
 void put(unsigned int address, int value){
 	mem[address] = value;
-	int pageNum = (i*32)/psize;
-	//check if pageNum is in current working set
-	isInSet = 0;
-	for(int i = 0; i < winsize; ++i){ //change winsize later to actual size of set
-		if(set[i]==pageNum){
-			//Base case: pageNum is in the set
-			isInSet = 1;
-			break;
-		}
-	}
-	if(!isInSet){
-		//put it in the set - nothing here yet!
-	}
-	//check if counter has reached windowsize
-	counter+=1;
-	if (counter==winsize){
-		//copy & store working set into some stash somewhere
-		for(i = 0; i < len(set[i]); ++i){
-			workingsetmodels[amountofws][i] = set[i];
-			//also clear the set
-			set[i] = 0;
-		}		
-	}
+	setCheck(1+(address*32)/pagesize);
 }
 
 void get(unsigned int address){
-	int pageNum = (i*32)/psize;
-	//check if pageNum is in current working set
-	isInSet = 0;
-	int i;
-	for(i = 0; i < winsize; ++i){ //change winsize later to actual size of set
-		if(set[i]==pageNum){
-			//Base case: pageNum is in the set
-			isInSet = 1;
-			break;
-		}
-	}
-	if(!isInSet){
-		//put it in the set - nothing here yet!
-	}
-	//check if counter has reached windowsize
-	counter+=1;
-	if (counter==winsize){
-		//copy & store working set into some stash somewhere
-		for(i = 0; i < len(set[i]); ++i){
-			workingsetmodels[amountofws][i] = set[i];
-			//also clear the set
-			set[i] = 0;
-		}		
-	}
+	setCheck(1+(address*32)/pagesize);
 	return mem[address];
 }
 
