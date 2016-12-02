eea9997756d9ead430d8a2f826b17c085ca754b0
TheBurnerGuy <TheBurnerGuy@users.noreply.github.com>
2016-12-01 23:55:08 -0700

    pivot is done and tested
    doing a few more tests to make sure, but is functioning without sim

--- quicksort.c
+++ quicksort.c
@@ -9,65 +9,99 @@ void swap(int* A, int* B){
 }
 
 int partition(int* A, int begin, int end){
-	pivotIndex = begin + ((end-begin)/2);
-	pivot = A[pivotIndex];
+	int pivotIndex = begin + ((end-begin)/2);
+	int pivot = A[pivotIndex];
+	swap(A+pivotIndex,A+end);
+	//swap pivot to end
+	int i = begin;
+	int j = end;
+	--end;
 	while(begin <= end){
 		if(A[begin] >= pivot && A[end] < pivot){
-			swap(A[begin], A[end]);
+			swap(A+begin, A+end);
 		}
-		if(A[begin] < pivot) begin++;
-		if(A[end] >= pivot) end--;
+		if(A[begin] < pivot) ++begin;
+		if(A[end] >= pivot) --end;
 	}
-	return begin;
+	//swap pivot back in place
+	for(i; i<j; ++i){
+		if(A[i]>A[j]){
+			swap(A+i,A+j);
+			break;
+		}
+	}
+	return i;
 }
 
 /*quicksort(): in-place implementation of quick sort
-Uses select() to find a pivot point such that partitioning on both sides of the pivot only needs O(logn) memory.
-Partitioning is done with partition()
+Partitioning and finding a pivot point is done with partition()
 While running in an infinite while loop, use a small array of indexes to keep track of partitions.
 In array, numbers are placed in this order as number ofpartitions grow:{begin1, end1, begin2, end2,...,begin(n), end(n)}
 The index for this array is incremented/decremented by 2 when going up/down (simulating returning from recursion)
 */
-void quicksort(int* A){
+void quicksort(int* A, int begin, int end){
 	int partitionIndex[1000]; //Note: Real simulator will have pretty much infinite storage, so remember to 'replace' the 1000.
-	int pivot, temp, i, indexPos = 0, begin = 0, end = len(A)-1;
+	int pivot, temp, i, indexPos = 0;
 	//Initialize partitionIndex + pivot to begin loop iterations
 	partitionIndex[0] = begin;
 	partitionIndex[1] = end;
 	while(indexPos >= 0){
+		//printf("%d ",indexPos);
 		//read next partition to sort
 		begin = partitionIndex[indexPos];
 		end = partitionIndex[indexPos+1];
+		pivot = partition(A, begin, end);
+		printf("Before:");
+		for(i = 0; i<11; ++i){
+			printf("%d ",A[i]);
+		}
+		printf("B: %d E: %d ",begin,end);
 		//Base case: Partition has only one number
 		if(end-begin==0){
-			posIndex -= 2;
+			indexPos -= 2;
+			printf("\n");
 			continue;
 		}
 		//Base case: Partition has two numbers
 		if(end-begin==1){
-			if(A[begin]<A[end]){
+			if(A[begin]>A[end]){
 				//Swap the two
-				temp = A[begin];
-				A[begin] = A[end];
-				A[end] = temp;
+				printf("Swapped %d and %d\n", A[begin], A[end]);
+				swap(A+begin, A+end);
 			}
-			posIndex -= 2;
+			indexPos -= 2;
 			continue;
 		}
 		//Else, find pivot and partition
 		pivot = partition(A, begin, end);
+		printf("Pivot:%d ",pivot);
+		printf("After:");
+		for(i = 0; i<11; ++i){
+			printf("%d ",A[i]);
+		}
+		printf("B: %d E: %d\n",begin,end);
 		//place partitions in a stack to be sorted later; smaller one to reduce stack size
 		if(pivot-begin>=end-pivot){
-			partitionIndex[posIndex] = begin;
-			partitionIndex[posIndex+1] = i-1;
-			partitionIndex[posIndex+2] = i+1;
-			partitionIndex[posIndex+3] = end;
+			partitionIndex[indexPos] = begin;
+			partitionIndex[indexPos+1] = pivot-1;
+			partitionIndex[indexPos+2] = pivot;
+			partitionIndex[indexPos+3] = end;
 		}else{
-			partitionIndex[posIndex] = i+1;
-			partitionIndex[posIndex+1] = end;
-			partitionIndex[posIndex+2] = begin;
-			partitionIndex[posIndex+3] = i-1;
+			partitionIndex[indexPos] = pivot;
+			partitionIndex[indexPos+1] = end;
+			partitionIndex[indexPos+2] = begin;
+			partitionIndex[indexPos+3] = pivot-1;
 		}
-		posIndex+=2;
+		indexPos+=2;
 	}
+}
+
+int main(){
+	int numberArray[11] = {9, 3, 5, 8, 66, 7, 1, 3, 4, 16, 2};
+	quicksort(numberArray,0,10);
+	int i;
+	for(i = 0; i<11; ++i){
+		printf("%d ",numberArray[i]);
+	}
+	printf("\n");
 }
